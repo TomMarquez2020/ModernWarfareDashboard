f2f842662574153d42021823af6f2841a8597846
unknown <TomMarquez2020@gmail.com>
2020-03-17 21:58:24 -0800

    Added logic to get data from api

--- modernwarfaredashboard/src/Components/Dashboard.js
+++ modernwarfaredashboard/src/Components/Dashboard.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Dashboard() {
-  return <ul></ul>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://my-json-server.typicode.com/TomMarquez2020/ModernWarfareDashboard/db"
+      )
+      .then(res => {
+        console.log(res);
+        setData(res.data.posts);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []); //second parameter to prevent infinite calls
+
+  return (
+    <ul>
+      {data.map(item => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default Dashboard;
